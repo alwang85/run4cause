5cb58509547b6b4680349e1e017cb605d4049b0d
Christian Cueto <ccueto90@gmail.com>
2015-05-06 15:54:22 -0400

    Added name to User schema, name is passed and saved from Jawbone JSON

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,12 +1,15 @@
 'use strict';
 var router = require('express').Router();
-module.exports = router;
-
-router.use('/tutorial', require('./tutorial'));
-router.use('/members', require('./members'));
+module.exports = function(app){
+    router.use('/tutorial', require('./tutorial'));
+    router.use('/members', require('./members'));
+    router.use('/jawbone', require('./jawbone')(app));
 
 // Make sure this is after all of
 // the registered routes!
-router.use(function (req, res) {
-    res.status(404).end();
-});
+    router.use(function (req, res) {
+        res.status(404).end();
+    });
+    return router;
+};
+
