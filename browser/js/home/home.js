79f7990979a7adaf794cc089b2d341cbf3be3ebd
Steve Kwak <steveguac@gmail.com>
2015-05-07 16:07:15 -0400

    folder changes

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -2,6 +2,15 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
-        templateUrl: 'js/home/home.html'
+        templateUrl: 'js/home/home.html',
+        controller: 'HomeController'
     });
+});
+
+app.controller('HomeController', function($http, $scope){
+    $scope.getFitbitData = function(){
+        $http.get('api/fitbit/getUserSteps').then(function(response){
+            console.log(response.data);
+        })
+    }
 });
