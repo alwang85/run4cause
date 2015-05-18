47106addc20224f627aa3abbd5715e702ba6127d
Alex Wang <alwang85@gmail.com>
2015-05-18 18:10:07 -0400

    added sanitization and links in confirmation msg to watsi

--- browser/js/app.js
+++ browser/js/app.js
@@ -1,5 +1,5 @@
 'use strict';
-var app = angular.module('FullstackGeneratedApp', ['ui.router', 'fsaPreBuilt', 'satellizer', 'ui.bootstrap']);
+var app = angular.module('FullstackGeneratedApp', ['ui.router', 'fsaPreBuilt', 'satellizer', 'ui.bootstrap', 'ngSanitize']);
 
 // base url provider
 app.config(function ($urlRouterProvider, $locationProvider) {
