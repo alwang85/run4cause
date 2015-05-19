1c084bd0ef7212a556eb12259cff7d255ee65f72
Steve Kwak <steveguac@gmail.com>
2015-05-19 16:28:18 -0400

    profile page update

--- browser/js/common/factories/User.js
+++ browser/js/common/factories/User.js
@@ -1,18 +1,69 @@
 app.factory('UserFactory', function($http, $q, $auth) {
-    var currentUserLogs = {
-        distance: 0,
-        calories: 0,
-        steps: 0,
-        startDate: null,
-        endDate: null
+    var currentUserLogs = function(logData){
+        var userLogByMetrics = {
+            distance: [],
+            calories: [],
+            steps: [],
+            sleep: []
+        };
+        var filteredDate = [];
+        var filteredQty = [];
+        logData.forEach(function(eachLog){
+            eachLog.metrics.forEach(function(eachMetric){
+                userLogByMetrics[eachMetric.measurement].push({
+                    date: new Date(eachLog.date),
+                    qty: eachMetric.qty
+                })
+            })
+        });
+        return userLogByMetrics;
     };
+    var sortArrayByDate = function(filteredLog){
+        return filteredLog.sort(function(a,b){
+            return a.date - b.date;
+        });
+    };
+
+    var sortAndSliceDate = function(filteredLog){
+        return sortArrayByDate(filteredLog).map(function(each){
+            return each.date.toString().slice(4,10);
+        });
+    };
+
+    var sortAndFilterQty = function(filteredLog, metric){
+        return sortArrayByDate(filteredLog).map(function(each){
+            if(metric==='distance') return (each.qty/1609.34).toFixed(1);
+            else return each.qty.toFixed(0);
+        });
+    };
+
+    var aggregateUserLogByCategory = function(userLog){
+        var objToReturn = {
+            startDate: null,
+            endDate: null,
+            distance: 0,
+            steps: 0,
+            calories: 0,
+            sleep: 0
+        };
+
+        for(var metric in userLog){
+            objToReturn.startDate = userLog[metric][0].date;
+            objToReturn.endDate = userLog[metric][userLog[metric].length-1].date;
+            userLog[metric].forEach(function(eachDay){
+                if(metric === 'distance') objToReturn[metric] += (eachDay.qty/1609.34);
+                else objToReturn[metric] += +eachDay.qty;
+            });
+        };
+        return objToReturn;
+    };
+
     var createNewUser = function(newUser) {
         return $http.post('/api/user/signup', newUser)
             .then(function(response) {
                 return response.body;
             })
             .catch(function(response) {
-                console.log(response);
                 return $q.reject({message : 'Unable to Sign Up'});
             });
     };
@@ -57,6 +108,10 @@ app.factory('UserFactory', function($http, $q, $auth) {
     var availableDevices = ['jawbone', 'fitbit'];
 
     return {
+        sortArrayByDate: sortArrayByDate,
+        aggregateUserLogByCategory: aggregateUserLogByCategory,
+        sortAndFilterQty: sortAndFilterQty,
+        sortAndSliceDate: sortAndSliceDate,
         currentUserLogs: currentUserLogs,
         createNewUser : createNewUser,
         availableDevices : availableDevices,
