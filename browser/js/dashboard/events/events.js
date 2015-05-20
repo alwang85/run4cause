519c15b9a563ab00da7f7dac80cd22a2841ff4e3
Alex Wang <alwang85@gmail.com>
2015-05-20 15:52:31 -0400

    added sockets to personal dashboard

--- browser/js/dashboard/events/events.js
+++ browser/js/dashboard/events/events.js
@@ -1,15 +1,23 @@
 'use strict';
-app.controller('EventsController', function($scope, Event, AuthService) {
+app.controller('EventsController', function($scope, Event, AuthService, SocketFactory) {
     var currentUser;
-    AuthService.getLoggedInUser().then(function(user) {
-        currentUser = user;
-        return user;
-    }).then(function(user) {
-        return Event.getAllEvents();
-    }).then(function(events){
-        var filtered = events.filter(function(eachEvent) {
-            return eachEvent.creator._id == currentUser._id;
-        });
-        $scope.events = filtered;
+     var getEvent = function(){
+       AuthService.getLoggedInUser().then(function(user) {
+         currentUser = user;
+         return user;
+       }).then(function(user) {
+         return Event.getAllEvents();
+       }).then(function(events){
+         var filtered = events.filter(function(eachEvent) {
+           return eachEvent.creator._id == currentUser._id;
+         });
+         $scope.events = filtered;
+       });
+     };
+    getEvent();
+    var socket = SocketFactory.getSocket();
+
+    socket.on('eventsChange', function(events) {
+      getEvent();
     });
 });
