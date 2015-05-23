cacec36e20106fce3744fba9b12e2b808d32a61c
Alex Wang <alwang85@gmail.com>
2015-05-23 13:23:54 -0400

    some jsdata is magically working!

--- browser/js/nonprofit/nonprofit.js
+++ browser/js/nonprofit/nonprofit.js
@@ -15,27 +15,59 @@ app.config(function ($stateProvider) {
     $stateProvider.state('nonProfit.detail', {
         url: '/:nonProfitToken',
         templateUrl: 'js/nonprofit/partial/detail.html',
-        controller: function($scope, NonProfitFactory, $state, $stateParams){
-            NonProfitFactory.getNonprofit($stateParams.nonProfitToken).then(function(patient){
-                $scope.patient = patient;
+        controller: function($scope, Patient, $state, $stateParams){
+          if (!$scope.patient){
+            console.log('thhese are the passed params', $stateParams.nonProfitToken)
+            Patient.find($stateParams.nonProfitToken).then(function(patient){
+              $scope.patient = patient;
             });
+          }
+            //Patient.getNonprofit($stateParams.nonProfitToken).then(function(patient){
+            //    $scope.patient = patient;
+            //});
         }
     });
 });
 
 
-app.controller('NonProfitController', function ($scope, AuthService, NonProfitFactory, $state) {
-    $scope.nonprofits= [];
-    $scope.patient = {};
+app.controller('NonProfitController', function ($scope, AuthService, DS, $state, Patient) {
+    //DS.findAll('patient').then(function(patients){
+    //  console.log(patients);
+    //  $scope.nonprofits = patients;
+    //}); didn't work!!
 
-    NonProfitFactory.getNonprofits()
-    .then(function(nonprofits) {
-        $scope.nonprofits = nonprofits;
-    });
+    Patient.findAll().then(function(patients){
+      console.log(patients);
+      $scope.nonprofits = patients;
+    })
     $scope.getPatient = function(patientId){
-      NonProfitFactory.getNonprofit(patientId).then(function(patient){
-        $scope.patient = patient;
-        $state.go("nonProfit.detail",{nonProfitToken:patientId});
-      });
+        Patient.find(patientId).then(function(patient){
+          console.log('foundPatient', patient);
+          $scope.patient = patient;
+          $state.go("nonProfit.detail",{nonProfitToken:patientId});
+        });
     };
+
+
+
+
+
+
+
+
+
+
+    //$scope.nonprofits= [];
+    //$scope.patient = {};
+    //
+    //NonProfitFactory.getNonprofits()
+    //.then(function(nonprofits) {
+    //    $scope.nonprofits = nonprofits;
+    //});
+    //$scope.getPatient = function(patientId){
+    //  NonProfitFactory.getNonprofit(patientId).then(function(patient){
+    //    $scope.patient = patient;
+    //    $state.go("nonProfit.detail",{nonProfitToken:patientId});
+    //  });
+    //};
 });
